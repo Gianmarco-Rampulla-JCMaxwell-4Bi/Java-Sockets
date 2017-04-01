0003d76a3c0a872287c78d1e0f55d64c0399fe0d
Gianmarco-Rampulla-JCMaxwell-4Bi <gianmarco.rampulla@jcmaxwell.it>
2017-04-01 13:11:32 +0200

    Update to ServerTestoMultiThreaded
    Update to ServerTestoMultiThreaded to adapt it with the new request of groups
    
    Changes:
    - Added a list in order to save any group that will be created and handle them
    - Added various method for managing the list of GroupClass
    - Changes to the code for making it compatible with a single connection (Server - Client) and a group connection in the same moment

--- debug/Java-Sockets-Server/ServerTestoMultiThreaded.java
+++ debug/Java-Sockets-Server/ServerTestoMultiThreaded.java
@@ -1,41 +1,40 @@
+package server;
+
 /*
  * ServerTesto MultiThreaded.java Server che attende per richieste di connessioni da Clients
  * e li gestisce in modo contemporaneo generando un socket "worker" per ogni connessione.
  * 
  */
 
-/**
- *
- * @author Prof. Matteo Palitto
- */
-
-
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import javaapplication7.KeyListen;
 
 
 public class ServerTestoMultiThreaded {
-
-    public static void main(String[] args) {
     
+    public static List<GroupClass> gruppi = new ArrayList<GroupClass>();
+    public static List<String> utenti = new ArrayList<String>();
+    
+    public static void main(String[] args) {
+       
+        
         Scanner s = new Scanner(System.in);
         
-        /*if (args.length != 1) {
-            System.out.println("Uso: java ServerTestoMultithreaded <Porta Server>");
-            return;
-        }*/
-        
+        System.out.print("Inserisci numero porta: ");
         int portNumber = s.nextInt();
-         KeyListen C = new KeyListen(); //creo ascoltatore comandi
-
+        KeyListen C = new KeyListen(); //creo ascoltatore comandi
+        
+        
         try{
             ServerSocket server = new ServerSocket(portNumber);
             System.out.println("Server di Testo in esecuzione...  (CTRL-C quits)\n");
              C.start(); //faccio partire il thread per l'ascolto
             while(true){
                 SocketWorker w;
+                
                 try {
                     //server.accept returns a client connection
                     w = new SocketWorker(server.accept());
@@ -53,4 +52,39 @@ public class ServerTestoMultiThreaded {
 
         
     }
+    
+    public static boolean checkGroup(String nome)
+    {
+        for(int i=0; i<gruppi.size(); i++)
+        {
+            if(gruppi.get(i).getNomeGruppo().equals(nome))
+                return true;
+        }
+        return false;
+    }
+    
+    public static boolean ControllaNickname(String nick)
+    {
+        for(int i=0; i<gruppi.size(); i++)
+        {
+            if(utenti.get(i).equals(nick))
+                return true;
+        }
+        return false;
+    }
+    
+    public static void printUtentiFromNomeGruppo(String nomeGruppo, PrintWriter out)
+    {
+        for(int i=0; i<gruppi.size(); i++)
+        {
+            if(gruppi.get(i).getNomeGruppo().equals(nomeGruppo))
+            {
+                for(int j=0; j<gruppi.get(i).getListaSocket().size(); j++)
+                {
+                    out.println(gruppi.get(i).getListaSocket().get(j));
+                }
+            }
+        }
+    }
+
 }
